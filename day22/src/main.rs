430fded88d85b314dc8708529fc050ceb079c246
Maja Kądziołka <maya@compilercrim.es>
2022-12-26 00:54:46 +0100

    Day 22 part 2 fucking finally
    Persecuted-By: Eric Wastl <eric@was.tl>

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,23 +1,15 @@
 use std::fs;
 use std::path::Path;
 use ndarray::Array2;
+use enum_map::{Enum, EnumMap, enum_map};
 
 type Pos = (usize, usize);
-type Delta = (isize, isize);
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Command {
     TurnLeft,
     TurnRight,
     Walk(u32),
-}
-
-fn turn_right((dx, dy): Delta) -> Delta {
-    (-dy, dx)
-}
-
-fn turn_left((dx, dy): Delta) -> Delta {
-    (dy, -dx)
 }
 
 fn parse_commands(s: &str) -> Vec<Command> {
@@ -44,7 +36,282 @@ fn parse_commands(s: &str) -> Vec<Command> {
     commands
 }
 
-fn testcase(fname: impl AsRef<Path>) {
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Enum)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    const ALL: [Direction; 4] = [
+        Direction::Up,
+        Direction::Down,
+        Direction::Left,
+        Direction::Right,
+    ];
+
+    fn cw(self) -> Direction {
+        use Direction::*;
+
+        match self {
+            Up => Right,
+            Right => Down,
+            Down => Left,
+            Left => Up,
+        }
+    }
+
+    fn ccw(self) -> Direction {
+        use Direction::*;
+
+        match self {
+            Up => Left,
+            Left => Down,
+            Down => Right,
+            Right => Up,
+        }
+    }
+
+    fn opposite(self) -> Direction {
+        use Direction::*;
+
+        match self {
+            Up => Down,
+            Down => Up,
+            Left => Right,
+            Right => Left,
+        }
+    }
+
+    fn as_delta(self, m: i32) -> (i32, i32) {
+        use Direction::*;
+
+        match self {
+            Up => (0, -m),
+            Down => (0, m),
+            Left => (-m, 0),
+            Right => (m, 0),
+        }
+    }
+}
+
+type FaceId = usize;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+struct Edge {
+    face: FaceId,
+    dir: Direction,
+}
+
+#[derive(Debug)]
+struct Face {
+    input_coords: (i32, i32),
+    edges: EnumMap<Direction, Option<Edge>>,
+    connections_handled: EnumMap<Direction, bool>,
+}
+
+#[derive(Debug)]
+struct Mesh {
+    faces: Vec<Face>,
+    face_size: i32,
+}
+
+impl Mesh {
+    fn new(face_size: i32) -> Mesh {
+        Mesh {
+            faces: vec![],
+            face_size,
+        }
+    }
+
+    fn id_for(&self, input_coords: (i32, i32)) -> Option<FaceId> {
+        self.faces
+            .iter()
+            .position(|face| face.input_coords == input_coords)
+    }
+
+    fn add_face(&mut self, input_coords: (i32, i32)) {
+        let mut face = Face {
+            input_coords,
+            edges: enum_map! {
+                _ => None,
+            },
+            connections_handled: enum_map! {
+                _ => false,
+            },
+        };
+
+        let face_id = self.faces.len();
+
+        for (dir, edge) in face.edges.iter_mut() {
+            let (x, y) = input_coords;
+            let (dx, dy) = dir.as_delta(self.face_size);
+            if let Some(f) = self.id_for((x + dx, y + dy)) {
+                *edge = Some(Edge {
+                    face: f,
+                    dir: dir.opposite(),
+                });
+
+                self.faces[f].edges[dir.opposite()] = Some(Edge {
+                    face: face_id,
+                    dir,
+                });
+            }
+        }
+
+        self.faces.push(face);
+    }
+
+    fn resolve_at(&mut self, face: FaceId, dir: Direction) -> bool {
+        let Some(edge1) = self.faces[face].edges[dir]
+            else { return false };
+        let Some(edge2) = self.faces[face].edges[dir.cw()]
+            else { return false };
+
+        let dir1 = edge1.dir.ccw();
+        let dir2 = edge2.dir.cw();
+
+        let set = |slot: &mut Option<Edge>, edge: Edge| {
+            match *slot {
+                None => *slot = Some(edge),
+                Some(e) => assert_eq!(e, edge),
+            }
+        };
+
+        set(&mut self.faces[edge1.face].edges[dir1], Edge {
+            face: edge2.face,
+            dir: dir2,
+        });
+
+        set(&mut self.faces[edge2.face].edges[dir2], Edge {
+            face: edge1.face,
+            dir: dir1,
+        });
+
+        true
+    }
+
+    fn resolve(&mut self) {
+        loop {
+            let mut all_resolved = true;
+            let mut made_progress = false;
+
+            for face in 0..self.faces.len() {
+                for dir in Direction::ALL {
+                    if !self.faces[face].connections_handled[dir] {
+                        if self.resolve_at(face, dir) {
+                            made_progress = true;
+                            self.faces[face].connections_handled[dir] = true;
+                        } else {
+                            all_resolved = false;
+                        }
+                    }
+                }
+            }
+
+            if all_resolved {
+                break;
+            } else if !made_progress {
+                dbg!(&self);
+                panic!("Resolving failed");
+            }
+        }
+    }
+
+    fn neighbour(&self, (x, y): Pos, dir: Direction) -> (Pos, Direction) {
+        use Direction::*;
+
+        let x = x as i32;
+        let y = y as i32;
+
+        let split = |a| {
+            let smol = a % self.face_size;
+            (a - smol, smol)
+        };
+
+        let (x0, x) = split(x);
+        let (y0, y) = split(y);
+
+        let (dx, dy) = dir.as_delta(1);
+        let x = x + dx;
+        let y = y + dy;
+
+        if (0..self.face_size).contains(&x) && (0..self.face_size).contains(&y) {
+            return (((x + x0) as usize, (y + y0) as usize), dir);
+        }
+
+        let face = self.id_for((x0, y0)).unwrap();
+
+        let end = self.face_size - 1;
+
+        let (edge, t) = if y < 0 {
+            (self.faces[face].edges[Up], x)
+        } else if y >= self.face_size {
+            (self.faces[face].edges[Down], end - x)
+        } else if x < 0 {
+            (self.faces[face].edges[Left], end - y)
+        } else if x >= self.face_size {
+            (self.faces[face].edges[Right], y)
+        } else {
+            unreachable!()
+        };
+
+        let edge = edge.unwrap();
+
+        let (x0, y0) = self.faces[edge.face].input_coords;
+        let (x, y) = match edge.dir {
+            Up => (end - t, 0),
+            Down => (t, end),
+            Left => (0, t),
+            Right => (end, end - t),
+        };
+
+        (((x + x0) as usize, (y + y0) as usize), edge.dir.opposite())
+    }
+}
+
+fn walk(
+    cmds: &[Command],
+    grid: &Array2<u8>,
+    neighbour: impl Fn(Pos, Direction) -> (Pos, Direction),
+) -> usize {
+    let mut y = 0;
+    let mut x = (0..).find(|&x| grid[[x, y]] != b' ').unwrap();
+    let mut dir = Direction::Right;
+
+    for &cmd in cmds {
+        match cmd {
+            Command::TurnLeft => dir = dir.ccw(),
+            Command::TurnRight => dir = dir.cw(),
+            Command::Walk(n) => {
+                for _ in 0..n {
+                    let (next, next_dir) = neighbour((x, y), dir);
+                    if grid[next] == b'#' {
+                        break;
+                    }
+                    (x, y) = next;
+                    dir = next_dir;
+                }
+            }
+        }
+    }
+
+    use Direction::*;
+    let facing = match dir {
+        Right => 0,
+        Down => 1,
+        Left => 2,
+        Up => 3,
+    };
+
+    let x = x + 1;
+    let y = y + 1;
+    1000 * y + 4 * x + facing
+}
+
+fn testcase(fname: impl AsRef<Path>, face_size: usize) {
     let fname = fname.as_ref();
     println!("{fname:?}");
     let data = fs::read_to_string(fname).unwrap();
@@ -63,56 +330,40 @@ fn testcase(fname: impl AsRef<Path>) {
         }
     }
 
-    let width = width as isize;
-    let height = height as isize;
+    let w = width as i32;
+    let h = height as i32;
 
-    let mut y = 0;
-    let mut x = (0..).find(|&x| grid[[x, y]] != b' ').unwrap();
-    let mut dx = 1;
-    let mut dy = 0;
-
-    let neighbour = |(mut x, mut y): Pos, (dx, dy): Delta| -> Pos {
+    let answer = walk(&cmds, &grid, |(mut x, mut y), dir| {
         loop {
-            x = (x as isize + dx).rem_euclid(width) as usize;
-            y = (y as isize + dy).rem_euclid(height) as usize;
+            let (dx, dy) = dir.as_delta(1);
+            x = (x as i32 + dx).rem_euclid(w) as usize;
+            y = (y as i32 + dy).rem_euclid(h) as usize;
             if grid[[x, y]] != b' ' {
-                return (x, y);
+                return ((x, y), dir);
             }
         }
-    };
+    });
 
-    for cmd in cmds {
-        match cmd {
-            Command::TurnLeft => (dx, dy) = turn_left((dx, dy)),
-            Command::TurnRight => (dx, dy) = turn_right((dx, dy)),
-            Command::Walk(n) => {
-                for _ in 0..n {
-                    let next = neighbour((x, y), (dx, dy));
-                    if grid[next] == b'#' {
-                        break;
-                    }
-                    (x, y) = next;
-                }
+    println!("Part 1: {answer}");
+
+    let mut mesh = Mesh::new(face_size as i32);
+
+    for x0 in (0..width as usize).step_by(face_size) {
+        for y0 in (0..height as usize).step_by(face_size) {
+            if grid[[x0, y0]] != b' ' {
+                mesh.add_face((x0 as i32, y0 as i32));
             }
         }
     }
 
-    let facing = match (dx, dy) {
-        (1, 0) => 0,
-        (0, 1) => 1,
-        (-1, 0) => 2,
-        (0, -1) => 3,
-        _ => unreachable!(),
-    };
+    mesh.resolve();
 
-    let x = x + 1;
-    let y = y + 1;
-    let answer = 1000 * y + 4 * x + facing;
+    let answer = walk(&cmds, &grid, |pos, dir| mesh.neighbour(pos, dir));
 
-    println!("Part 1: {answer}");
+    println!("Part 2: {answer}");
 }
 
 fn main() {
-    testcase("example.in");
-    testcase("puzzle.in");
+    testcase("example.in", 4);
+    testcase("puzzle.in", 50);
 }
